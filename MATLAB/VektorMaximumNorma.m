bf4a1c703adb9a2c4b657fa687377d7db08fe872
Nagy Máté(U3ROFS) <90890704+LoQi03@users.noreply.github.com>
2022-10-18 23:34:18 +0200

    wip

--- MATLAB/VektorMaximumNorma.m
+++ MATLAB/VektorMaximumNorma.m
@@ -1,14 +1,9 @@
-A=rand(2,3)
-n=size(A)
+A=rand(1,7)
+n=size(A,2);
 max=0;
-for i=1:n(1)
-    sum=0;
-    for j=1:n(2)
-        sum=sum+abs(A(i,j));
+for i=1:n
+    if(max<abs(A(i)))
+        max=abs(A(i));
     end
-    if max<sum
-        max=sum;
-    end
-    sum=0;
 end
 max
--- MATLAB/VektorosszegNorma.m
+++
@@ -1,9 +0,0 @@
-A=rand(1,7)
-n=size(A,2);
-max=0;
-for i=1:n
-    if(max<abs(A(i)))
-        max=abs(A(i));
-    end
-end
-max
